fea2a8f6c917a60ced5c1db7efe53db2505fa430
geekymoose <constantin.masson@umontreal.ca>
2019-12-10 15:58:10 +0100

    Import new InputSystem package

---
+++ Assets/Scripts/GameDebugger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDebugger : MonoBehaviour
+{
+    private void Awake()
+    {
+        if(!Debug.isDebugBuild)
+        {
+            // Script enabled ONLY in debug build
+            this.gameObject.GetComponent<GameDebugger>().enabled = false;
+            return;
+        }
+        Debug.LogWarning("GamePlayer Debugger activated");
+    }
+}
