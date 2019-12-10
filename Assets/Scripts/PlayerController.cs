97e823546ac18fa028403a44f56ce258bb9887fa
geekymoose <constantin.masson@umontreal.ca>
2019-12-10 19:44:31 +0100

    Create a super basic movement controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveDistanceInUnityUnits = 100.0f;
+    public float moveDurationInSec = 0.5f;
+
+    private float moveReloadAccumulatorInSec = 0.0f;
+    private Vector2 moveDirection;
+
+    public void OnInputMove(InputAction.CallbackContext context)
+    {
+        this.moveDirection = context.ReadValue<Vector2>();
+        this.moveDirection.Normalize();
+
+        if(this.moveDirection != Vector2.zero && this.moveReloadAccumulatorInSec <= 0.0f)
+        {
+            this.moveReloadAccumulatorInSec = this.moveDurationInSec;
+            this.transform.Translate(this.moveDirection * this.moveDistanceInUnityUnits * Time.fixedDeltaTime);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        this.moveReloadAccumulatorInSec -= Time.fixedDeltaTime;
+    }
+}
+
