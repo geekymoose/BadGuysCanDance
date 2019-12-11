d91660ca3e3d1d436fdf997660dbe41f280651ef
jdsoundleft <jdsoundleft@ubisoft.com>
2019-12-11 17:57:17 +0100

    Callback Ok in main Scene in Unity

---
+++ Assets/Play_Beat.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play_Beat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayBeat()
+    {
+        AkSoundEngine.PostEvent("Play_Beep", gameObject);
+    }
+}
