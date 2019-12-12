fa5f25ca630b21164fff1b7094ffa4ed9215f7db
jdsoundleft <jdsoundleft@ubisoft.com>
2019-12-12 13:58:14 +0100

    update soundbanks

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,10 +14,22 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID PLAY_BEEP = 2063165418U;
+        static const AkUniqueID PLAY_DEATH = 1172822028U;
+        static const AkUniqueID PLAY_SFX_DEATH_BIBEND = 186166397U;
+        static const AkUniqueID PLAY_SFX_DEATH_JASON = 3196001096U;
+        static const AkUniqueID PLAY_SFX_DEATH_JOKER = 1405572140U;
+        static const AkUniqueID PLAY_SFX_DEATH_PENNYWISE = 3491373059U;
+        static const AkUniqueID PLAY_SFX_DEATH_SHREDDER = 3935204712U;
+        static const AkUniqueID PLAY_SFX_DEATH_SKELETOR = 4252615758U;
+        static const AkUniqueID PLAY_SFX_DEATH_TERMINATOR = 1156841624U;
+        static const AkUniqueID PLAY_SFX_DEATH_VADER = 1318223257U;
+        static const AkUniqueID PLAY_SFX_LASER_HIT = 2681111161U;
+        static const AkUniqueID PLAY_SFX_LASER_SHOOT = 3678284003U;
         static const AkUniqueID PLAY_SW_MUSIC = 932654524U;
         static const AkUniqueID SET_STATE_END = 2300403011U;
         static const AkUniqueID SET_STATE_MENU = 2302270969U;
         static const AkUniqueID SET_STATE_MENU_INMUSIC = 3055616074U;
+        static const AkUniqueID SET_STATE_PHASE0 = 1799566027U;
         static const AkUniqueID SET_STATE_PHASE1 = 1799566026U;
         static const AkUniqueID SET_STATE_PHASE2 = 1799566025U;
         static const AkUniqueID SET_STATE_PHASE3 = 1799566024U;
@@ -33,6 +45,7 @@ namespace AK
             {
                 static const AkUniqueID ST_MUS_END = 1264120226U;
                 static const AkUniqueID ST_MUS_MENU = 3450655090U;
+                static const AkUniqueID ST_MUS_PHASE0 = 1231023908U;
                 static const AkUniqueID ST_MUS_PHASE1 = 1231023909U;
                 static const AkUniqueID ST_MUS_PHASE2 = 1231023910U;
                 static const AkUniqueID ST_MUS_PHASE3 = 1231023911U;
@@ -40,6 +53,27 @@ namespace AK
         } // namespace ST_MUS
 
     } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace SW_DEATH_CHARACTER
+        {
+            static const AkUniqueID GROUP = 3979372760U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID SW_DEATH_BIBENDUM = 971196139U;
+                static const AkUniqueID SW_DEATH_JASON = 3477344376U;
+                static const AkUniqueID SW_DEATH_JOKER = 1723165980U;
+                static const AkUniqueID SW_DEATH_PENNYWISE = 2043537171U;
+                static const AkUniqueID SW_DEATH_SHREDDER = 1098586552U;
+                static const AkUniqueID SW_DEATH_SKELETOR = 1994578398U;
+                static const AkUniqueID SW_DEATH_TERMINATOR = 2368303816U;
+                static const AkUniqueID SW_DEATH_VADER = 3755072937U;
+            } // namespace SWITCH
+        } // namespace SW_DEATH_CHARACTER
+
+    } // namespace SWITCHES
 
     namespace BANKS
     {
@@ -49,11 +83,11 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID DEATH = 779278001U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID MUSIC = 3991942870U;
         static const AkUniqueID SFX = 393239870U;
         static const AkUniqueID UI = 1551306167U;
-        static const AkUniqueID VOICE = 3170124113U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
