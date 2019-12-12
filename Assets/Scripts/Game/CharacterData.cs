265fc6ebfac14cdd27abd5b7c8f803052800b962
geekymoose <constantin.masson@umontreal.ca>
2019-12-12 11:45:53 +0100

    Update character data with all sprites and short description

--- Assets/Scripts/Game/CharacterData.cs
+++ Assets/Scripts/Game/CharacterData.cs
@@ -3,5 +3,10 @@
 [CreateAssetMenu(fileName = "CharacterData", menuName = "ScriptableObjects/CharacterData", order = 1)]
 public class CharacterData : ScriptableObject
 {
-    public Sprite sprite;
+    public Sprite spriteIdle1;
+    public Sprite spriteIdel2;
+    public Sprite spriteKilled;
+
+    public string characterName;
+    public string characterDescription;
 }
