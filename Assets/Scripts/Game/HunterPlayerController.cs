58d2d37fef97451d935ab8faa639de858dfd0e55
geekymoose <constantin.masson@umontreal.ca>
2019-12-12 16:43:27 +0100

    Add a reload speed for hunter

--- Assets/Scripts/Game/HunterPlayerController.cs
+++ Assets/Scripts/Game/HunterPlayerController.cs
@@ -3,8 +3,37 @@ using UnityEngine.InputSystem;
 
 public class HunterPlayerController : MonoBehaviour
 {
+    [Tooltip("Reload speed in seconds")]
+    [SerializeField]
+    private float reloadSpeedInSec = 0.5f;
+
+    private float reloadAccumulatorInSec = 0.0f;
+
+
     public void OnInputFire(InputAction.CallbackContext context)
     {
+        this.Fire();
+    }
+
+    private void Update()
+    {
+        this.reloadAccumulatorInSec -= Time.deltaTime;
+    }
+
+    public bool CanFire()
+    {
+        return this.reloadAccumulatorInSec <= 0.0f;
+    }
+
+    private void Fire()
+    {
+        if (!this.CanFire())
+        {
+            return;
+        }
+
+        this.reloadAccumulatorInSec = this.reloadSpeedInSec;
+
         AkSoundEngine.PostEvent("Play_SFX_Laser_Shoot", gameObject);
 
         Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
