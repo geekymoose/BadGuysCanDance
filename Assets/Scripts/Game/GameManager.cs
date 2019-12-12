db984079d4abf0be111c39c932981ae4baf18e2b
geekymoose <constantin.masson@umontreal.ca>
2019-12-12 11:18:42 +0100

    Add phases in game sounds

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -10,6 +10,7 @@ public class GameManager : MonoBehaviour
 
     private Conductor conductor;
     private SnapGridCharacter player;
+    private int countCharactersAtStart = 8;
     private int countCharacters = 8; // Hard coded to 8 characters
 
 
@@ -21,18 +22,26 @@ public class GameManager : MonoBehaviour
         Assert.IsNotNull(this.gameOverEvent, "Missing asset");
         Assert.IsNotNull(this.conductor, "Missing asset");
 
-        // Select a random player
-        float randValue = Random.Range(0, 10000);
-        int indice = (int)(randValue % countCharacters);
-        this.player = this.listAIPlayers[indice].GetComponent<SnapGridCharacter>();
-        this.player.UsePlayerControls();
-        this.listAIPlayers.RemoveAt(indice);
+        this.SelectRandomPlayer();
 
         // Place All AI in brick
         foreach (SnapGridAIController currentAI in this.listAIPlayers)
         {
             this.conductor.AddBrick(new Brick_SnapGridMoveAI(currentAI));
         }
+
+        // Start game sound
+        AkSoundEngine.PostEvent("Set_State_Phase1", gameObject);
+    }
+
+    public void SelectRandomPlayer()
+    {
+        // Select a random player
+        float randValue = Random.Range(0, 10000);
+        int indice = (int)(randValue % countCharacters);
+        this.player = this.listAIPlayers[indice].GetComponent<SnapGridCharacter>();
+        this.player.UsePlayerControls();
+        this.listAIPlayers.RemoveAt(indice);
     }
 
     public void OnPlayerKilledEvent()
@@ -41,11 +50,20 @@ public class GameManager : MonoBehaviour
         Debug.Log("OnPlayerKilledEvent received");
         countCharacters--;
         this.gameOverEvent.Raise();
+        AkSoundEngine.PostEvent("Set_State_End", gameObject);
     }
 
     public void OnAIKilledEvent()
     {
-        countCharacters--;
         Debug.Log("OnPlayerKilledEvent received");
+        countCharacters--;
+        if(countCharacters == 4)
+        {
+            AkSoundEngine.PostEvent("Set_State_Phase2", gameObject);
+        }
+        else if(countCharacters == 2)
+        {
+            AkSoundEngine.PostEvent("Set_State_Phase3", gameObject);
+        }
     }
 }
